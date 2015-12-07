b9e2ee73dc5cafdcd68579794e8601f6c4ea975b
lplus <lplus@126.com>
2015-12-07 18:16:07 +0800

    add example hello

---
+++ example/hello/main.d
@@ -0,0 +1,17 @@
+import std.concurrency;
+import fcgi.stdio;
+
+void main() 
+{
+	init();
+    while(accept)
+    {
+    	"Content-Type: text/html; charset=UTF-8\r\n"
+    	"\r\n"
+    	.write;
+    	
+    	writeln("<h1">Hello FastCGI </h1>\n");
+    	
+    	finish;
+    }
+}
